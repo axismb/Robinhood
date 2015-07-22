eafd28c571407a30c6ab27c36ab90db08c2baa7c
azizmb9494 <aziz@azizmb.com>
2015-07-22 14:40:49 -0400

    Added Authentication & User classes.

--- Robinhood/Classes.cs
+++ Robinhood/Classes.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Robinhood
 {
-    class Classes
+    class Authentication
     {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+        [JsonProperty("non_field_errors")]
+        public string[] Errors { get; set; }
+    }
+
+    public class User
+    {
+        [JsonProperty("guid")]
+        public string ID { get; set; }
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
     }
 }
